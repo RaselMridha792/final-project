2392abdd6d2ca2d3d010aa8f37b31e944d7331a6
rasel mridha <raselmridha792@gmail.com>
2024-12-28 20:37:45 +0600

    ready bisto client site basic strucures installing all necessary components and libraries

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+import daisyui from 'daisyui';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Inter': ['Inter', 'serif'],
+        'Cinzel': ['Cinzel', 'serif'],
+
+      },
+    },
+  },
+  plugins: [daisyui,],
+}
+
